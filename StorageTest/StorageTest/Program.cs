825265aa148b6c767708884c75e7cc308afd3c0e
Aluno Fatec <aluno@fateclab.net>
2026-03-20 11:23:38 -0300

    project upload

---
+++ StorageTest/StorageTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StorageTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
