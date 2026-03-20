344d31cc65eda6af35a9c722ffec70ae5e334457
João Gabriel Vasques Ribeiro <joaogabrielvasques16@gmail.com>
2026-03-20 11:00:09 -0300

    Implement GeneralStorage class for generic storage

---
+++ GeneralStorage.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageTest
+{
+    internal class GeneralStorage
+    {
+        public Dictionary<string, object> generalStorage;
+        public GeneralStorage() {
+            this.generalStorage = new Dictionary<string, object>();
+        }
+        public void AddStorage<T>(string name)
+        {
+            var newStorage = new Storage<T>();
+            this.generalStorage.Add(name, newStorage);
+        }
+        public Storage<T> GetStorage<T>(string name)
+        {
+            return (Storage<T>) this.generalStorage.GetValueOrDefault(name);
+        }
+        public Dictionary<string, object> GetGeneralStorage()
+        {
+            return this.generalStorage;
+        }
+
+    }
+}
