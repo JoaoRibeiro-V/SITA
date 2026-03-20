84ad4bb331d1ee365b7e26b756cfc7206ec0cb81
João Gabriel Vasques Ribeiro <joaogabrielvasques16@gmail.com>
2026-03-20 10:59:33 -0300

    Implement basic storage functionality in Program.cs

---
+++ Program.cs
@@ -0,0 +1,35 @@
+namespace StorageTest
+{
+    internal class Program
+    {
+        public static GeneralStorage storage;
+        private static void registerStorages()
+        {
+            storage = new GeneralStorage();
+            storage.AddStorage<Aluno>("Aluno");
+        }
+        static void Main(string[] args)
+        {
+            registerStorages();
+
+            Aluno exemplo1 = new Aluno();
+            exemplo1.Name = "Fulano";
+            Aluno exemplo2 = new Aluno();
+            exemplo2.Name = "Ciclano";
+
+            Storage<Aluno> alunosStorage = storage.GetStorage<Aluno>("Aluno");
+            alunosStorage.AddData(exemplo1.Name, exemplo1);
+
+            foreach (var aluno in alunosStorage.storages) {
+                Console.WriteLine(aluno.Key);
+            }
+
+
+        }
+
+        public class Aluno
+        {
+            public string? Name { get; set; }
+        }
+    }
+}
