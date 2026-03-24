556545034fbc5ed3bf795299f05cf0516f645563
Aluno Fatec <aluno@fateclab.net>
2026-03-24 11:00:33 -0300

    Storage

---
+++ StorageTest/StorageTest/GeneralStorage.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageTest
+{
+    internal class GeneralStorage
+    {
+        public Dictionary<string, object> generalStorage;
+        public GeneralStorage()
+        {
+            this.generalStorage = new Dictionary<string, object>();
+        }
+        public void AddStorage<T>()
+        {
+            var newStorage = new Storage<T>();
+            this.generalStorage.Add(typeof(T).Name, newStorage);
+        }
+        public Storage<T> GetStorage<T>()
+        {
+            return (Storage<T>)this.generalStorage.GetValueOrDefault(typeof(T).Name);
+        }
+        public Dictionary<string, object> GetGeneralStorage()
+        {
+            return this.generalStorage;
+        }
+
+    }
+}
