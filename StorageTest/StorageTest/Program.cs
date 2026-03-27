6eac5a08f047806e9e0fe6b78c0aad0d4aaac7ca
Aluno Fatec <aluno@fateclab.net>
2026-03-27 11:31:13 -0300

    classes melhoradas

--- StorageTest/StorageTest/Program.cs
+++ StorageTest/StorageTest/Program.cs
@@ -11,7 +11,7 @@ namespace StorageTest
         {
             storage = new GeneralStorage();
             storage.AddStorage<Aluno>();
-            storage.AddStorage<Diretor>();
+            storage.AddStorage<User>();
         }
         static void Main(string[] args)
         {
@@ -20,6 +20,7 @@ namespace StorageTest
             Aluno exemplo1 = new Aluno();
             exemplo1.Name = "Fulano";
             exemplo1.Idade = 5;
+            exemplo1.RA = 1201212;
             Aluno exemplo2 = new Aluno();
             exemplo2.Name = "Ciclano";
             exemplo2.Idade = 15;
@@ -29,13 +30,18 @@ namespace StorageTest
             diretor1.cpf = "53111";
 
             Storage<Aluno> alunosStorage = storage.GetStorage<Aluno>();
-            Storage<Diretor> diretorStorage = storage.GetStorage<Diretor>();
+            Storage<User> userStorage = storage.GetStorage<User>();
 
-            alunosStorage.AddData(exemplo1.Name, exemplo1);
+            alunosStorage.AddData(exemplo1.RA, exemplo1);
             alunosStorage.AddData(exemplo2.Name, exemplo2);
             diretorStorage.AddData(diretor1.cpf, diretor1);
 
             Aluno get = alunosStorage.GetData("Fulano");
+
+            
+            // VALIDAR RA //     
+            alunosStorage.TryGetValue(exemplo1.RA, out get); 
+
             Console.WriteLine(get.Idade);
             get.Idade = 6;
             Console.WriteLine(alunosStorage.GetData("Fulano").Idade);
@@ -48,14 +54,22 @@ namespace StorageTest
 
         public class Aluno : User{
             public string? RA {  get; set; }
-            public string? nomeResponsavel { get; set; }
-        }
-        public class Diretor : User{
-            public string? cpf { get; set; }
+            public string[]? nomeResponsavel { get; set; }
+            public string[]? telResponsavel { get; set; }
+            public string[]? condEspeciais { get; set; }
+            public string[]? alergias { get; set; }
+            public string? religiao { get; set; }
+            public string? nomePediatra { get; set; }
+
+            
+
         }
         public class User{
             public string? Name { get; set; }
             public int? Idade { get; set; }
+            public string? cpf { get; set; }           
+            public string? dataNascimento { get; set; }
+            public int? userType { get; set; }
         }
     }
 }
